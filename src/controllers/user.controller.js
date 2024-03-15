97322782faa7a1d29e0c99843b231f77d572aaf9
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-15 10:39:13 +0100

    user.controller basic structure ok

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,35 @@
+
+// router.get('/api/users'); // ver todos los usuarios super_admin
+// router.get('/api/users/profile'); //ver perfil usuario
+// router.put('/api/users'); //modificar perfil al menos un campo
+
+
+const getAllUsers = (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+
+
+};
+
+const getUsersProfile =(req,res)=>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+
+const updateUsersProfile = (res,res)=> {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+};
