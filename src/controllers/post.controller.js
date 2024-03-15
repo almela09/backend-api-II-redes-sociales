d9936cff41f45e2e91e64e123dcbbe858d663e6b
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-15 10:35:55 +0100

    basic structure of post.controller done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -7,4 +7,69 @@
 // router.get('/api/posts');   //recuperar todos los posts
 // router.get('/api/posts/:_id');   //recuperar posts por id
 // router.get('/api/posts/:user_id');   //recuperar posts de un usuario
-// router.get('api/users/posts/user_id')
+
+
+const createPost = (req,res)=> {
+try {
+    
+} catch (error) {
+    
+}
+
+};
+
+const deleteById = (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const updatePostById = (req,res)=>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const myOwnPost = (req,res)=>{
+try {
+    
+} catch (error) {
+    
+}
+
+};
+
+const getAllPost = (req,res)=>{
+
+try {
+    
+} catch (error) {
+    
+}
+
+};
+
+const getPostById =(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+
+
+};
+
+const getUserPostById = (req,res)=>{
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+};
