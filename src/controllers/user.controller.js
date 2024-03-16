c54c2a72b4a72d4a26e8e4829c7912e6b66cc60c
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-16 17:14:19 +0100

    update user.controller: getAllUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,10 +5,36 @@ import User from "../models/User"
 // router.put('/api/users'); //modificar perfil al menos un campo
 
 //ver TODOS los usuarios: super_admin
-const getAllUser = (req, res) => {
+export const getAllUser = async (req, res) => {
     try {
 
+        if (req.user && req.user.role === 'super_admin') {
+            const users = await User.find({});
+            res.json(users);
+        } else {
+            //devolver aqui el error si no es super_admin
+            res.status(403).json(
+                {
+                    success: false,
+                    message: 'access denied, only super_admin allowed'
+
+
+                }
+            );
+
+        }
+
     } catch (error) {
+
+        res.status(500).json(
+            {
+                success: false,
+                message: 'users not found',
+                error: error
+
+
+            }
+        );
 
     }
 
