8a325d8a82e99086dc4011aeb378d552d8bcbb1d
almela09 <145797671+almela09@users.noreply.github.com>
2024-04-08 17:33:22 +0200

    feature: cors

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ import express from "express";
 import 'dotenv/config'
 import { dbConnection } from "./database/db.js";
 import router from './routes/router.js'
-
+import cors from "cors"
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 4001;
 
